1bd892d6b78adaa452f5b5ed6476d4fe3a626207
Anita RASOA <anita.ras@onja.org>
2020-06-13 08:25:34 +0300

    Trying to fixe the journale entry

--- journal.js
+++ journal.js
@@ -1,15 +1,8 @@
 
 let Journallist = {
-    "Title": {
-        "title1": "First blog letter",
-        "title2": "Today was a good day",
-        "title3": "Another day at onja"
-    },
-    "Content": {
-        "content1": "This blog is about what happended last week during the election time.",
-        "content2": "This blog is about what happended last week during the election time.",
-        "content3": "This blog is about what happended last week during the election time."
-    }
+    "Title": ["First blog letter", "Today was a good day", "Another day at onja"],
+    "Content": [ "This blog is about what happended last week during the election time.", "This blog is about what happended last week during the election time.",
+                "This blog is about what happended last week during the election time."]
 }
 
 
@@ -18,9 +11,9 @@ while (continueProgram === '') {
 
     let journalTypes = prompt('Welcome to my Onja journal\n Choose(1) for listing all the entries \n Choose(2) for adding a new entry \n Choose(3) to quite');
     if (journalTypes === '1') {
-        alert(`Title: ${Journallist.Title.title1} \n Content: ${Journallist.Content.content1}`);
-        alert(`Title: ${Journallist.Title.title2} \n Content: ${Journallist.Content.content2}`);
-        alert(`Title: ${Journallist.Title.title3} \n Content: ${Journallist.Content.content3}`);
+        alert(`Title: ${Journallist.Title[0]} \n Content: ${Journallist.Content[0]}`);
+        alert(`Title: ${Journallist.Title[1]} \n Content: ${Journallist.Content[1]}`);
+        alert(`Title: ${Journallist.Title[2]} \n Content: ${Journallist.Content[2]}`);
     } else if (journalTypes === '2') {
         var  newJournal = prompt('Enter your journal title');
         var  newJournal = prompt('Enter your journal content');  
@@ -29,7 +22,12 @@ while (continueProgram === '') {
         } else if (newJournal === 'content') {
             newJournal = prompt('Enter your journal content'); 
         }
-         
-    }
         continueProgram = '';
+    }
+} 
+
+let journalTypes = prompt('Welcome to my Onja journal\n Choose(1) for listing all the entries \n Choose(2) for adding a new entry \n Choose(3) to quite');
+if (journalTypes === '3') {
+  var journal = 0;
+  alert('Thank you');
 }
