9c3efa3e759d52d7c0b6d0f0a95371801df15581
Anita RASOA <anita.ras@onja.org>
2020-06-15 10:51:15 +0300

    Adding the new entry in the object

--- journal.js
+++ journal.js
@@ -1,33 +1,55 @@
 
-let Journallist = {
-    "Title": ["First blog letter", "Today was a good day", "Another day at onja"],
-    "Content": [ "This blog is about what happended last week during the election time.", "This blog is about what happended last week during the election time.",
-                "This blog is about what happended last week during the election time."]
-}
+const journallist = [
+    { title: "First blog letter",
+      content: "This blog is about what happended last week during the election time."}, 
+      
+    { title: "Today was a good day",
+      content: "This blog is about what happended last week during the election time."},
+               
+    { title: "Another day at onja",
+      content: "This blog is about what happended last week during the election time."}
+]
 
-
-let continueProgram = '';
-while (continueProgram === '') {
-
-    let journalTypes = prompt('Welcome to my Onja journal\n Choose(1) for listing all the entries \n Choose(2) for adding a new entry \n Choose(3) to quite');
-    if (journalTypes === '1') {
-        alert(`Title: ${Journallist.Title[0]} \n Content: ${Journallist.Content[0]}`);
-        alert(`Title: ${Journallist.Title[1]} \n Content: ${Journallist.Content[1]}`);
-        alert(`Title: ${Journallist.Title[2]} \n Content: ${Journallist.Content[2]}`);
-    } else if (journalTypes === '2') {
-        var  newJournal = prompt('Enter your journal title');
-        var  newJournal = prompt('Enter your journal content');  
-        if (newJournal === 'title') {
-            newJournal = prompt('Enter your journal title'); 
-        } else if (newJournal === 'content') {
-            newJournal = prompt('Enter your journal content'); 
+let choice = prompt('Welcome to my Onja journal\n Choose(1) for listing all the entries \n Choose(2) for adding a new entry \n Choose(3) to quite \n choose(4) to delete');
+    while (choice !== '3') {
+        for(let i = 0; i < journallist.length; i++) {
+            if (choice === '1') {
+                alert('Title:' +' '+ journallist[i].title + '\n'+ 'Content:' + ' ' + journallist[i].content);
+            } else if (choice === '2') {
+                let newEntrytitle = prompt('Enter your journal title');
+                let newEntrycontent = prompt('Enter your journal content');
+                let newEntry = {title: newEntrytitle, content: newEntrycontent};
+                newEntry.newEntrytitle = newEntrytitle;
+                newEntry.newEntrycontent = newEntrycontent;
+                journallist.push(newEntry);
+                for(let i = 0; i < journallist.length; i++) {
+                    alert('Title:' +' '+ journallist[i].title + '\n'+ 'Content:' + ' ' + journallist[i].content);
+                }
+          
+            } else if (choice === '3') {
+              alert('thank you');
+            } else if (choice === '4') {
+                let deleteEntrytitle = prompt('Enter your journal title');
+                let deleteEntrycontent = prompt('Enter your journal content');
+                let deleteEntry = {title: deleteEntrytitle, content: deleteEntrycontent};
+                deleteEntry.deleteEntrytitle = deleteEntrytitle;
+                deleteEntry.deleteEntrycontent = deleteEntrycontent;
+                journallist.pop(deleteEntry);
+                for(let i = 0; i < journallist.length; i++) {
+                    alert('Title:' +' '+ journallist[i].title + '\n'+ 'Content:' + ' ' + journallist[i].content);
+                }
+            }
         }
-        continueProgram = '';
+        choice = '';
     }
-} 
-
-let journalTypes = prompt('Welcome to my Onja journal\n Choose(1) for listing all the entries \n Choose(2) for adding a new entry \n Choose(3) to quite');
-if (journalTypes === '3') {
-  var journal = 0;
-  alert('Thank you');
-}
+/*
+        let newTitle = prompt('Enter your new title');
+        let newContent = prompt('Enter your new content');
+        let newEntry = {title: newTitle, content: newContent}
+        newEntry.newTitle = newTitle;
+        newEntry.newContent = newContent;
+        journalEntries.push(newEntry);
+        for (let i = 0; i < journalEntries.length; i++) {
+            alert(`Title: ${journalEntries[i].title} \n Content: ${journalEntries[i].content}`);
+        }
+        */
