cf81ac53197868d9c7acfd282edbcca745e73eb1
Anita RASOA <anita.ras@onja.org>
2020-06-16 15:57:30 +0300

    Fixing All the mistakes with my journal

--- journal.js
+++ journal.js
@@ -1,5 +1,5 @@
-
-const journallist = [
+//These are the journal list 
+const journal = [
     { title: "First blog letter",
       content: "This blog is about what happended last week during the election time."}, 
       
@@ -9,36 +9,28 @@ const journallist = [
     { title: "Another day at onja",
       content: "This blog is about what happended last week during the election time."}
 ]
-let choice = 0;
-while (choice !== '3') {
-    let choice = prompt('Welcome to my Onja journal\n Choose(1) for listing all the entries \n Choose(2) for adding a new entry \n Choose(3) to quite \n choose(4) to delete \n Choose(5) to delete a spesific entry');
-    for(let i = 0; i < journallist.length; i++) {
-            if (choice === '1') {
-                alert('Title:' +' '+ journallist[i].title + '\n'+ 'Content:' + ' ' + journallist[i].content);
-            } else if (choice === '2') {
-                let newEntrytitle = prompt('Enter your journal title');
-                let newEntrycontent = prompt('Enter your journal content');
-                let newEntry = {title: newEntrytitle, content: newEntrycontent};
-                newEntry.newEntrytitle = newEntrytitle;
-                newEntry.newEntrycontent = newEntrycontent;
-                journallist.push(newEntry);
-                for(let i = 0; i < journallist.length; i++) {
-                    alert('Title:' +' '+ journallist[i].title + '\n'+ 'Content:' + ' ' + journallist[i].content);
-                }
-          
-            } else if (choice === '3') {
-              alert('thank you');
-            } else if (choice === '4') {
-                journallist.pop();
-                for(let i = 0; i < journallist.length; i++) {
-                    alert('Title:' +' '+ journallist[i].title + '\n'+ 'Content:' + ' ' + journallist[i].content);
-                }
-            } else if (choice === '5') {
-                journallist.splice(1, 1);
-               for(let i = 0; i < journallist.length; i++) {
-                alert('Title:' +' '+ journallist[i].title + '\n'+ 'Content:' + ' ' + journallist[i].content);
-            }
+
+let choice = prompt('Welcome to my Onja journal\n Choose(1) for listing all the entries \n Choose(2) for adding a new entry \n Choose(3) to quite \n choose(4) to delete \n Choose(5) to delete a spesific entry');
+while (choice !== '3') { // Return the menu
+    if (choice === '1') { // Loop though all the entries
+        for(let i = 0; i < journal.length; i++) {
+            alert('Title:' +' '+ journal[i].title + '\n'+ 'Content:' + ' ' + journal[i].content);
         }
+    } else if (choice === '2') { // Add new entry
+        let newEntrytitle = prompt('Enter your journal title');
+        let newEntrycontent = prompt('Enter your journal content');
+        let newEntry = {title: newEntrytitle, content: newEntrycontent};
+            journal.push(newEntry);
+          
+    } else if (choice === '3') { // We want to quit the program
+        alert('thank you');
+    } else if (choice === '4') { // Delete the last entry
+        const entryDeleted = journal.pop();
+        alert(`We delete the last entry ${entryDeleted.title}`);
+    } else if (choice === '5') { // Enter the index's entry you want to delete.
+        let indexTodelete = Number(prompt('Enter the index of the array you want to delete (1 to ${journal.lenth})'));
+        let deleteEntry = journal.splice(indexTodelete--, 1);
+        alert(`Index to delete ${deleteEntry[0].title} `);
     }
-    choice = '';
+    choice = choice = prompt('Welcome to my Onja journal\n Choose(1) for listing all the entries \n Choose(2) for adding a new entry \n Choose(3) to quite \n choose(4) to delete \n Choose(5) to delete a spesific entry');
 }
